1a4efe61de891958031ed42e68b6b43211bd3c3d
Gunbir Walia <gwaliammv@gmail.com>
2026-01-18 00:46:36 +0530

    first commit

---
+++ ecommerce-project/src/App.jsx
@@ -0,0 +1,40 @@
+import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { HomePage } from './pages/home/HomePage.jsx';
+import { CheckoutPage } from './pages/checkout/CheckoutPage.jsx';
+import { OrdersPage } from './pages/orders/OrdersPage.jsx';
+import { TrackingPage} from './pages/TrackingPage.jsx';
+import { NotFoundPage } from './pages/NotFoundPage';
+import './App.css'
+
+window.axios = axios;
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product')
+    setCart(response.data);
+  };
+
+  useEffect(() => {    
+    loadCart();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
+
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
+
+      <Route path="orders" element={<OrdersPage cart={cart} loadCart={loadCart} />} />
+
+      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
+
+      <Route path="*" element={<NotFoundPage cart={cart} />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -0,0 +1,124 @@
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { Link, useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import { Header } from '../components/Header';
+import './TrackingPage.css';
+
+export function TrackingPage({ cart }) {
+    const { orderId, productId } = useParams();
+
+    const [order, setOrder] = useState(null);
+
+    useEffect(() => {
+        const fetchTrackingData = async () => {
+            const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
+        };
+        fetchTrackingData();
+    }, [orderId]);
+
+    if (!order) {
+        return null;
+    }
+
+    const orderProduct = order.products.find((orderProduct) => {
+        return orderProduct.productId === productId;
+    });
+
+    const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+    const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+
+    let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+    if (deliveryPercent > 100) {
+        deliveryPercent = 100;
+    }
+
+    const isPreparing = deliveryPercent < 33;
+    const isShipped = deliveryPercent >= 33 && deliveryPercent < 100;
+    const isDelivered = deliveryPercent === 100;
+
+    return (
+        <>
+            <title>Tracking</title>
+            <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
+
+            <Header cart={cart} />
+
+            <div className="header">
+                <div className="left-section">
+                    <Link to="/" className="header-link">
+                        <img className="logo"
+                            src="images/logo-white.png" />
+                        <img className="mobile-logo"
+                            src="images/mobile-logo-white.png" />
+                    </Link>
+                </div>
+
+                <div className="middle-section">
+                    <input className="search-bar" type="text" placeholder="Search" />
+
+                    <button className="search-button">
+                        <img className="search-icon" src="images/icons/search-icon.png" />
+                    </button>
+                </div>
+
+                <div className="right-section">
+                    <Link className="orders-link header-link" to="/orders">
+
+                        <span className="orders-text">Orders</span>
+                    </Link>
+
+                    <Link className="cart-link header-link" to="/checkout">
+                        <img className="cart-icon" src="images/icons/cart-icon.png" />
+                        <div className="cart-quantity">3</div>
+                        <div className="cart-text">Cart</div>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="tracking-page">
+                <div className="order-tracking">
+                    <Link className="back-to-orders-link link-primary" to="/orders">
+                        View all orders
+                    </Link>
+
+                    <div className="delivery-date">
+                        {deliveryPercent >= 100 ? 'Delivered on ' : 'Arriving on '}
+                        {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+                    </div>
+
+                    <div className="product-info">
+                        {orderProduct.product.name}
+                    </div>
+
+                    <div className="product-info">
+                        Quantity: {orderProduct.quantity}
+                    </div>
+
+                    <img className="product-image" src={orderProduct.product.image} />
+
+                    <div className="progress-labels-container">
+                        <div className={`progress-label ${isPreparing && 'current-status'}`}>
+                            Preparing
+                        </div>
+
+                        <div className={`progress-label ${isShipped && 'current-status'}`}>
+                            Shipped
+                        </div>
+
+                        <div className={`progress-label ${isDelivered && 'current-status'}`}>
+                            Delivered
+                        </div>
+                    </div>
+
+                    <div className="progress-bar-container">
+                        <div className="progress-bar" style={{
+                            width: `${deliveryPercent}%`
+                        }}></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
